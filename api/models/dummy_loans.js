e2009eba5d05add568f5fb065ef5b7464494915c
Emmanuel Ayodele <emmayodayo@gmail.com>
2019-05-15 14:21:58 +0100

    add models

---
+++ api/models/dummy_loans.js
@@ -0,0 +1,83 @@
+export const loans = [
+    {
+        id: "QC-20178545",
+        token: "6pkP65ADt0avHsAx",
+        user: "tonystark@marvel.com",
+        status: "approved",
+        repaid: true,
+        tenor: 50,
+        amount: 7000,
+        balance: 4000,
+        interest: 15,
+        paymentInstallment: 10,
+    },
+
+    {
+        id: "QC-201785847",
+        token: "axXWNdpTa3UfjSW1",
+        user: "peterparker@marvel.com",
+        status: "rejected",
+        repaid: false,
+        tenor: 50,
+        amount: 8000,
+        balance: 4000,
+        interest: 15,
+        paymentInstallment: 10,
+    },
+
+    {
+        id: "QC-201785121",
+        token: "xfGn9mkEuNretCJR",
+        user: "peterparker@marvel.com",
+        status: "rejected",
+        repaid: false,
+        tenor: 50,
+        amount: 8000,
+        balance: 4000,
+        interest: 15,
+        paymentInstallment: 10,
+    },
+
+    {
+        id: "QC-201785127",
+        token: "w3TgysVuYkl0lNod",
+        user: "davidjackson@gmail.com",
+        status: "approved",
+        repaid: true,
+        tenor: 50,
+        amount: 4800,
+        balance: 4000,
+        interest: 15,
+        paymentInstallment: 10,
+    }
+]
+
+export const repayments = [
+    {
+        id: "QC-20178545",
+        token: E4G1fyzpaZt2oyEw,
+        installment: 10.6,
+        amount: 500,
+    },
+
+    {
+        id: "QC-201785127",
+        token: E4G1fyzpaZt2oyEw,
+        installment: 10.6,
+        amount: 500,
+    },
+
+    {
+        id: "QC-201785121",
+        token: E4G1fyzpaZt2oyEw,
+        installment: 10.6,
+        amount: 500,
+    },
+
+    {
+        id: "QC-201785847",
+        token: E4G1fyzpaZt2oyEw,
+        installment: 10.6,
+        amount: 500,
+    },
+];
---
+++ api/models/dummy_users.js
@@ -0,0 +1,78 @@
+export const users = [{
+        id: 'QC-20180514',
+        email: 'Johndoe@gmail.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'randompassword',
+        adress: 'Lagos, Nigeria',
+        status: 'Verified',
+        isAdmin: false,
+    },
+
+    {
+        id: 'QC-20180514',
+        email: 'Maryjane@gmail.com',
+        firstName: 'Mary',
+        lastName: 'Jane',
+        password: 'randompassword1',
+        adress: 'Kano, Nigeria',
+        status: 'Not-verified',
+        isAdmin: false,
+    },
+
+    {
+        id: 'QC-20180514',
+        email: 'AnthonyJoshua@gmail.com',
+        firstName: 'Anthony',
+        lastName: 'Joshua',
+        password: 'randompassword3',
+        adress: 'Abuja, Nigeria',
+        status: 'verified',
+        isAdmin: false,
+    },
+];
+
+export const dummyData = [{
+    id: 'QC-20180517',
+    email: 'AnthonyJoshua@gmail.com',
+    firstName: 'Anthony',
+    lastName: 'Joshua',
+    password: 'randompassword3',
+    adress: 'Abuja, Nigeria',
+    status: 'verified',
+    isAdmin: false,
+},
+
+{
+    id: 'QC-20180514',
+    email: 'JamesKnowles@gmail.com',
+    firstName: 'James',
+    lastName: 'Knowles',
+    password: 'randompassword3',
+    adress: 'Abuja, Nigeria',
+    status: 'verified',
+    isAdmin: true,
+},
+
+{
+    id: 'QC-20180514',
+    email: 'AnthonyJoshua@gmail.com',
+    firstName: 'Anthony',
+    lastName: 'Joshua',
+    password: 'randompassword3',
+    adress: 'Abuja, Nigeria',
+    status: 'verified',
+    isAdmin: false,
+},
+];
+
+export const admin = {
+    id: 'QK-588A979LL3M',
+    email: 'job@gmail.com',
+    firstName: 'job',
+    lastName: 'ogins',
+    password: 'december',
+    adress: 'kigali',
+    status: 'un-verified',
+    isAdmin: true,
+};
